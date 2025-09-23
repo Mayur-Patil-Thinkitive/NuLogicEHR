5d8afc686572381b2a241f2e32cdd2bc808523e6
hritiknagpuree <hritik.nagpure@thinkitive.com>
2025-09-23 19:35:33 +0530

    Add comment in patientTabel.

--- NuLogicEHR/Models/Patient.cs
+++ NuLogicEHR/Models/Patient.cs
@@ -2,9 +2,11 @@ namespace NuLogicEHR.Models
 {
     public class Patient
     {
+        //Id
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public string Email { get; set; }//email
+        public string Email { get; set; }
+
     }
 }
