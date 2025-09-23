cf10c96efd928d104737da59042d5494635fdb8b
hritiknagpuree <hritik.nagpure@thinkitive.com>
2025-09-23 18:32:28 +0530

    Initial commit

--- NuLogicEHR/Models/Patient.cs
+++ NuLogicEHR/Models/Patient.cs
@@ -5,6 +5,6 @@ namespace NuLogicEHR.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public string Email { get; set; }
+        public string Email { get; set; }//email
     }
 }
